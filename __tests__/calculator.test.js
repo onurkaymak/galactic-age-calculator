799c74484b5669f966c336fe7aa0266cc3d52628
onurkaymak <onurkaymak@outlook.com>
2023-09-01 08:17:12 -0700

    Initialize the project from template repo

---
+++ __tests__/calculator.test.js
@@ -0,0 +1,8 @@
+// import { Calculator } from './../src/js/calculator.js';
+
+// // describe('Function GOES HERE', () => {
+
+// //   test('EXPLAIN YOUR TEST HERE', () => {
+// //     // expect().toEqual(result);
+// //   });
+// // });
