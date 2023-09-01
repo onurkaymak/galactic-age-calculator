89990ed0a3701fc3b0e6311b3316788203f39da7
onurkaymak <onurkaymak@outlook.com>
2023-09-01 11:23:47 -0700

    Pass Test#4 for calcForPlanet func

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -9,14 +9,17 @@ export class Calculator {
         const mercuryYearInEarth = .24;
         const venusYearInEarth = .62;
         const marsYearInEarth = 1.88;
+        const jupiterYearInEarth = 11.86;
 
         let inMercuryYear = Math.round((this.earth.earthAge / mercuryYearInEarth));
         let inVenusYear = Math.round((this.earth.earthAge / venusYearInEarth));
         let inMarsYear = Math.round((this.earth.earthAge / marsYearInEarth));
+        let inJupiterYear = Math.round((this.earth.earthAge / jupiterYearInEarth));
 
         this.mercury = { ageInMercury: inMercuryYear };
         this.venus = { ageInVenus: inVenusYear };
         this.mars = { ageInMars: inMarsYear };
+        this.jupiter = { ageInJupiter: inJupiterYear };
     }
 }
 
