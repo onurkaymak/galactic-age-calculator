c3cdb4e8ec250093c5afd619cfa6d71d1330bab9
onurkaymak <onurkaymak@outlook.com>
2023-09-01 11:31:02 -0700

    Refactor file tree

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -8,35 +8,3 @@ describe('Calculator', () => {
     });
 });
 
-
-
-describe('calcForPlanet', () => {
-
-    test('Test #1 - Should return a calculated age in a created mercury object based on planet Mercury solar year.',
-        () => {
-            const calculator = new Calculator(32);
-            calculator.calcForPlanet();
-            expect(calculator.mercury).toEqual({ ageInMercury: 133 });
-        });
-
-    test('Test #2 - Should return a calculated age in a created venus object based on planet Venus solar year.',
-        () => {
-            const calculator = new Calculator(32);
-            calculator.calcForPlanet();
-            expect(calculator.venus).toEqual({ ageInVenus: 52 });
-        });
-
-    test('Test #3 - Should return a calculated age in a created mars object based on planet Mars solar year.',
-        () => {
-            const calculator = new Calculator(32);
-            calculator.calcForPlanet();
-            expect(calculator.mars).toEqual({ ageInMars: 17 });
-        });
-
-    test('Test #4 - Should return a calculated age in a created jupiter object based on planet Jupiter solar year.',
-        () => {
-            const calculator = new Calculator(32);
-            calculator.calcForPlanet();
-            expect(calculator.jupiter).toEqual({ ageInJupiter: 3 });
-        });
-});
