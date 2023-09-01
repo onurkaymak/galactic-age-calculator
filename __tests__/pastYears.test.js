b330013fbafffb78a6dbfbad0ff499ea33124c1f
onurkaymak <onurkaymak@outlook.com>
2023-09-01 14:10:54 -0700

    Create Test #3 for pastYears function.

--- __tests__/pastYears.test.js
+++ __tests__/pastYears.test.js
@@ -15,4 +15,12 @@ describe('pastYears', () => {
             calculator.pastYears(43);
             expect(calculator.mercury).toEqual({ yearPassedSince: { message: "It has been 54.17 years passed, since your birthday of 179 in planet Mercury!" } });
         });
+
+
+    test('Test #3 - Should return how many years have passed on planet Venus since a past birthday.',
+        () => {
+            const calculator = new Calculator(56);
+            calculator.pastYears(43);
+            expect(calculator.venus).toEqual({ yearPassedSince: { message: "It has been 20.96 years passed, since your birthday of 69 in planet Mercury!" } });
+        });
 });
