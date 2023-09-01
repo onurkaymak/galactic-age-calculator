003c6970d803b4187496475a42d132e6fc8728b6
onurkaymak <onurkaymak@outlook.com>
2023-09-01 15:14:41 -0700

    Create Test #2 for futureBirthday function.

--- __tests__/futureBirthday.test.js
+++ __tests__/futureBirthday.test.js
@@ -8,4 +8,12 @@ describe('futureBirthday', () => {
             calculator.futureBirthday(61);
             expect(calculator.earth.futureBirthday).toEqual({ message: "You have 5 years left to turn in 61 in planet Earth!" });
         });
+
+
+    test('Test #2 - Should return how many years left for a future birthday based on planet Mercury.',
+        () => {
+            const calculator = new Calculator(56);
+            calculator.futureBirthday(61);
+            expect(calculator.mercury).toEqual({ futureBirthday: { message: "You have 5 years left to turn in 61 in planet Earth!" } });
+        });
 });
